004a365af1d7c8a2ae5b97ac06823d6ab0bc2818
Sayed <sayemhasan110@gmail.com>
2023-08-22 16:47:55 +0100

    render plot

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Gantt, Tick, XAxis } from "./containers";
+import Plot from "./containers/plot";
 
 function App() {
   return (
     <div className="App" style={{ marginTop: 10 }}>
       <Gantt start={1672531200000} end={1680303600000}>
-        <XAxis>
+        <XAxis height={35}>
           {(ticks: any) => {
             return ticks.map((x: Date, i: number) => (
               <Tick key={i} start={x} alignmentBaseline="hanging"></Tick>
             ));
           }}
         </XAxis>
+        <Plot />
       </Gantt>
     </div>
   );
