4106f00a5c68c574d426e5fd4d79cb24186bcf98
Sayed <sayemhasan110@gmail.com>
2023-09-07 11:41:41 +0100

    Added fixed rows comp. validation

--- src/containers/group.tsx
+++ src/containers/group.tsx
@@ -1,4 +1,10 @@
-import { PropsWithChildren, createContext } from "react";
+import {
+  Children,
+  PropsWithChildren,
+  ReactElement,
+  createContext,
+} from "react";
+import { FixedRows } from "./fixed-rows";
 
 export const GroupContext = createContext<{ groupId: any; groupIndex: number }>(
   { groupId: "", groupIndex: 0 }
@@ -7,6 +13,17 @@ export const GroupContext = createContext<{ groupId: any; groupIndex: number }>(
 export default function Group(
   props: PropsWithChildren<{ id: string; index?: number }>
 ) {
+  const fixedRowsAndChildren = [];
+  let fixedRows = 0;
+  Children.forEach(props.children, (child) => {
+    const item = child as ReactElement<PropsWithChildren<{ index: number }>>;
+    if (item.type === FixedRows) {
+      fixedRowsAndChildren.push(item);
+      if (++fixedRows > 1)
+        throw new Error("You can have multi " + FixedRows.name + " component");
+    }
+  });
+
   return (
     <g>
       <GroupContext.Provider
